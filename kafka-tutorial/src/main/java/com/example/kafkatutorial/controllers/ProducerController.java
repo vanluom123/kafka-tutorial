1b1470be9f3ac4acae73d40e23522561c38b6e0e
Phan Van Luom <phanvanluom97bd@gmail.com>
2022-06-14 00:15:08 +0700

    update

--- kafka-tutorial/src/main/java/com/example/kafkatutorial/controllers/ProducerController.java
+++ kafka-tutorial/src/main/java/com/example/kafkatutorial/controllers/ProducerController.java
@@ -1,11 +1,11 @@
 package com.example.kafkatutorial.controllers;
 
+import com.example.kafkatutorial.model.Student;
 import com.example.kafkatutorial.transformers.MessageTransformer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/producer")
@@ -22,4 +22,18 @@ public class ProducerController {
     public void sendMessage(@RequestParam("message") String message) {
         transformer.sendMessage("student", message);
     }
+
+    @PostMapping("/send-message-2")
+    public void sendMessage(@RequestBody Student student) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String message;
+        try {
+            message = objectMapper.writeValueAsString(student);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        transformer.sendMessage("student", message);
+    }
+
+
 }
--- kafka-tutorial/src/main/java/com/example/kafkatutorial/listeners/MessageListener.java
+++ kafka-tutorial/src/main/java/com/example/kafkatutorial/listeners/MessageListener.java
@@ -1,19 +1,32 @@
 package com.example.kafkatutorial.listeners;
 
+import com.example.kafkatutorial.model.Student;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class MessageListener {
 
     private String msg;
 
-    public String getMsg() {
-        return msg;
-    }
+    private Student student;
 
     @KafkaListener(topics = {"student"}, groupId = "group-id")
     private void getMessage(String message) {
         msg = message;
+    }
+
+    @KafkaListener(topics = {"test"}, groupId = "group-id")
+    private void listen(String message) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            student = objectMapper.readValue(message, Student.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
